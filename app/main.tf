7b65f8795635b81f96afb7d513e3e58f1ec7e329
Matiixx <87618425+Matiixx@users.noreply.github.com>
2024-12-15 12:24:37 +0100

    change docker image to latest tag

--- app/main.tf
+++ app/main.tf
@@ -73,7 +73,7 @@ resource "google_cloud_run_v2_service" "tempfileshare_cloud_run_service" {
 
   template {
     containers{
-      image = "docker.io/cichostepski/tempfile-share:0.0.3"
+      image = "docker.io/cichostepski/tempfile-share:latest"
 
       env {
         name = "NEXT_PUBLIC_BACKEND_URL_PROD"
